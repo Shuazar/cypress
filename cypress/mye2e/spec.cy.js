1ee7100171e0a1496c551149b66d0b0b8712507d
Shuazar <shubaevazary@gmail.com>
2023-05-15 14:47:29 +0300

    Add contains

--- cypress/mye2e/spec.cy.js
+++ cypress/mye2e/spec.cy.js
@@ -1,3 +1,21 @@
+/// <reference types="cypress" />
+describe("tttt", () => {
+  it("", () => {
+    cy.visit("https://next.privat24.ua/mobile?/lang=en");
+    cy.contains("Вхід");
+  });
 
-/// <reference types="Cypress">
+  it("", () => {
+    cy.visit("https://next.privat24.ua/mobile?/lang=en");
+    cy.contains("div", "Вхід");
+  });
 
+  it("", () => {
+    cy.visit("https://next.privat24.ua/mobile?/lang=en");
+    cy.contains("div", "Вхід".toUpperCase(), { matchCase: false });
+  });
+  it.only("", () => {
+    cy.visit("https://next.privat24.ua/mobile?/lang=en");
+    cy.get('footer').contains('API',{matchCase:false});
+  });
+});
