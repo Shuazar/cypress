dbfe67927255807dbaf7b9067792563fb85bd150
Shuazar <shubaevazary@gmail.com>
2023-05-15 13:50:07 +0300

    Remove all code for master

--- cypress/mye2e/spec.cy.js
+++ cypress/mye2e/spec.cy.js
@@ -1,25 +1,3 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://next.privat24.ua/deposit/open");
-    cy.get("tbody").find("td").find("div").find("button").eq("0");
-  });
 
-  it("Using Get with Find Eq", () => {
-    cy.viewport(1700,800)
-    cy.visit("https://docs.cypress.io/api/commands/eq#syntax");
-    cy.get('aside').find('div').find('ul').find('li').find('a');
-  });
-
-  it("Using Get with Find Eq ", () => {
-    cy.viewport(17,800)
-    cy.visit("https://docs.cypress.io/api/commands/eq#syntax");
-    cy.get('aside').find('div').find('ul').find('li').find('a');
-  });
-  it("Using Get with Find Eq ", () => {
-    cy.viewport(1700,800)
-    cy.visit("https://docs.cypress.io/api/commands/eq#syntax");
-    cy.get('aside').find('div').find('ul').find('li').find('a');
-  });
-});
-
+/// <reference types="Cypress">
 
